073c16d346c3f109194f79ca003217ed58c43847
Nishikant <nishi62285>
2019-12-29 01:43:19 +0530

    Changing NameSpace

--- app/regionB/forms/regionb_form.py
+++ app/regionB/forms/regionb_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,BooleanField,RadioField,SelectField,DecimalField
 from wtforms.validators import DataRequired,Length
-from app.forms.osa_form import OSABaseForm
+from app.forms.region_form import OSABaseForm
 
 class REGIONBForm(OSABaseForm):
     exchanges=SelectField('Exchange',choices=[('EC','EC'),('CS','CS')])
